8a566ea9a2b3b6caa2b698c9d1cbae8f7a95ba73
Erik Dahlstrom <ErikD@magenic.com>
2019-09-05 13:41:45 -0500

    #004# Added updated Hello World text

--- 101TestApp/101TestApp/Program.cs
+++ 101TestApp/101TestApp/Program.cs
@@ -12,7 +12,16 @@ namespace _101TestApp
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" ,ggg,        gg                                          ,ggg,      gg      ,gg                                          ");
+            Console.WriteLine("dP``Y8b       88           ,dPYb, ,dPYb,                 dP``8a     88     ,8P                         ,dPYb,         8I ");
+            Console.WriteLine("Yb, `88       88           IP'`Yb IP'`Yb                 Yb, `88     88     d8'                         IP'`Yb         8I ");
+            Console.WriteLine(" ``  88       88           I8  8I I8  8I                  ``  88     88     88                          I8  8I         8I ");
+            Console.WriteLine("     88aaaaaaa88           I8  8' I8  8'                      88     88     88                          I8  8'         8I ");
+            Console.WriteLine("     88```````88   ,ggg,   I8 dP  I8 dP    ,ggggg,            88     88     88    ,ggggg,     ,gggggg,  I8 dP    ,gggg,8I ");
+            Console.WriteLine("     88       88  i8` `8i  I8dP   I8dP    dP`  `Y8ggg         88     88     88   dP`  `Y8ggg  dP````8I  I8dP    dP`  `Y8I ");
+            Console.WriteLine("     88       88  I8, ,8I  I8P    I8P    i8'    ,8I           Y8    ,88,    8P  i8'    ,8I   ,8'    8I  I8P    i8'    ,8I ");
+            Console.WriteLine("     88       Y8, `YbadP' ,d8b,_ ,d8b,_ ,d8,   ,d8'            Yb,,d8``8b,,dP  ,d8,   ,d8'  ,dP     Y8,,d8b,_ ,d8,   ,d8b,");
+            Console.WriteLine("     88       `Y8888P`Y8888P'`Y888P'`Y88P`Y8888P`               `88`    `88`   P`Y8888P`    8P      `Y88P'`Y88P`Y8888P``Y8");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
